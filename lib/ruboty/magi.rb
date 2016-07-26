1e4f3c6499b265c6eeaad9653b9926b69a1094c4
Shulla Cignoir <cignoir@gmail.com>
2016-07-26 11:35:46 +0900

    Simplify

--- lib/ruboty/magi.rb
+++ lib/ruboty/magi.rb
@@ -1,6 +1,23 @@
+require "ruboty"
 require "ruboty/magi/version"
 
 module Ruboty
-  module Magi
+  module Handlers
+    class Magi < Base
+      on(
+        /magi.*?/i,
+        name: 'answer',
+        description: 'Output result of deliberation'
+      )
+
+      def answer(message)
+        message.reply("MELCHIOR-1:[#{answer}] BALTHASAR-2:[#{answer}] CASPER-3:[#{answer}]")
+      end
+
+      private
+      def lot
+        %w(承認 否定).sample
+      end
+    end
   end
 end
